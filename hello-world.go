f076ccfd115a409bffa452a145286330ea5a5381
Roberto Wagner <robertowgsf@gmail.com>
2021-11-05 15:22:11 -0300

    Hello, World!

---
+++ hello-world.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+	fmt.Println("A wise man once said \"" + quote.Go() + "\" and GO was born.")
+}
